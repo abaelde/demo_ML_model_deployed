d5ee759ab79056f57fedaf3e0322b996eb69495c
abaelde <aurelien.baelde@gmail.com>
2022-07-19 16:20:22 +0200

    build the repo and prepared code for production use

---
+++ regression_model/predict.py
@@ -0,0 +1,34 @@
+import typing as t
+
+import numpy as np
+import pandas as pd
+
+from regression_model.config.core import config
+from regression_model.processing.data_manager import load_pipeline
+from regression_model.processing.validation import validate_inputs
+
+pipeline_file_name = f"{config.app_config.pipeline_save_file}.pkl"
+_price_pipe = load_pipeline(file_name=pipeline_file_name)
+
+
+def make_prediction(
+    *,
+    input_data: t.Union[pd.DataFrame, dict],
+) -> dict:
+    """Make a prediction using a saved model pipeline."""
+
+    data = pd.DataFrame(input_data)
+    validated_data, errors = validate_inputs(input_data=data)
+    results = {"predictions": None, "version": "0.0.1", "errors": errors}
+
+    if not errors:
+        predictions = _price_pipe.predict(
+            X=validated_data[config.model_config.features]
+        )
+        results = {
+            "predictions": [np.exp(pred) for pred in predictions],  # type: ignore
+            "version": "0.0.1",
+            "errors": errors,
+        }
+
+    return results
